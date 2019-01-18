7c9a74d38f64a40bf973a27cca77740c0fea44fa
Александр Александрович Голубев <aag@ganymed.factory.alexplus.ru>
2019-01-18 17:22:18 +0300

    for home

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,16 @@
 // #include "timers.h"
 #include "delay.h"
 #include "rus_string.h"
+#include "string_buffer.h"
 #include "hd44780.h"
+
+using E  = mcu::PB3;
+using RW = mcu::PD2;
+using RS = mcu::PC12;
+using DB4 = mcu::PB4;
+using DB5 = mcu::PB5;
+using DB6 = mcu::PB6;
+using DB7 = mcu::PB7;
 
 
 /// эта функция вызывается первой в startup файле
@@ -33,76 +42,21 @@ extern "C" void init_clock ()
       .set      (mcu::RCC::SystemClock::CS_PLL);
 }
 
+
+
+
+
 int main()
 {
-   auto up = mcu::Button::make<mcu::PB8>();
-   // auto down = mcu::Button::make<mcu::PB9>();
-   // auto delay = mcu::Delay::make<mcu::Periph::TIM4>(); 
-   auto& led3 = Pin::make<mcu::PC7, mcu::PinMode::Output>();
-   auto& led2 = Pin::make<mcu::PC6, mcu::PinMode::Output>();
-   // // auto led5 = mcu::Pin::make_new<mcu::PC9, mcu::PinMode::Output>();
-   auto& led1 = Pin::make<mcu::PB12, mcu::PinMode::Output>();
-   auto& lcd = HD44780::make<mcu::PC12, mcu::PD2, mcu::PB3, mcu::PB4, mcu::PB5, mcu::PB6, mcu::PB7>();
-   // auto& screen = LCD{};
-//    lcd.instruction(0x84);
+   String_buffer lcd;
+   HD44780::make<RS, RW, E, DB4, DB5, DB6, DB7>(lcd.get_buffer());
+   lcd.center() << "Hello, World!" << next_line;
    lcd.center() << "Alex_Plus";
-   lcd.line(1).center() << 2019;
-   // lcd.line(2).center() << string;
-   lcd.line(2).center() << string_1;
-   lcd.line(3).center() << ":";
-//    lcd.set_line(1).central() << "Alex_Plus Ultrasound company";
-//    lcd.set_line(2).central() << string;
-//    lcd.set_line(0).set_cursor(5) << "Alex_Plus";
-//    lcd.set_line(1).central() << string_1;
-   // lcd << "Hello, World! Alex_Plus Ultrasound company";
-//    lcd << "It was on the corner of the street that he noticed the first sign of\
-// something peculiar - a cat reading a map. For a second, Mr. Dursley\
-// didn't realize what he had seen -- then he jerked his head around to\
-// look again.";
-   // lcd.set_line(2).central() << "Hello, World!";
-//    lcd.set_line(3).central() << 587;
    
-   // lcd << "Alex_";
-   //  lcd.set_cursor(2) << "Hello, World!";
-   int p {0};
-   int m {0};
-   // lcd << p;
-   // lcd << "Hello, World! Alex_Plus - Ultrasound company";
-
+   
+   
    while (1) {
-      // lcd.line(2).center() << string;
-      led3 = led2 = led1 = true;
-      delay<50000>();
-      led3 = led2 = led1 = false; 
-      delay<50000>();
-      led3 = led2 = led1 = true;
-      delay<50000>();
-      led3 = led2 = led1 = false;
-      delay<50000>();
-      led3 = led2 = led1 = true;
-      delay<200000>();
-      led3 = led2 = led1 = false;
-      delay<200000>();
-      led3 = led2 = led1 = true;
-      delay<200000>();
-      led3 = led2 = led1 = false;
-      delay<200000>();
-      led3 = led2 = led1 = true;
-      delay<200000>();
-      led3 = led2 = led1 = false;
-      delay<200000>();
-
-
-      // lcd.line(2).center() << string_1;
-      // if (up)
-      //    lcd << " - Ultrasound company";
-      lcd.line(3).cursor(10) << p;
-      p++;
-      if (p > 60) {p = 1; m++;lcd.line(3).cursor(10) << "0 ";}
-      if (m > 60) {m = 0; lcd.line(3).cursor(7) << " ";}
-      if (m < 10)
-         lcd.line(3).cursor(8) << m;
-      else lcd.line(3).cursor(7) << m;
+      
    }
 
    
