2b42cc380dd9c6036ce13ea1fe4e79a48d8bdf2d
slonegd <slonkdv@mail.ru>
2019-05-11 14:30:58 +0300

    work with screens: start main + debug set_screen

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,8 @@
 #define F_OSC   8000000UL
 #define F_CPU   72000000UL
 
-#include "stm32f103xb.h"
+#define HEAP_SIZE 1024
+
 #include "init_clock.h"
 #include "main.h"
 #include "timers.h"
@@ -10,7 +11,13 @@
 #include "string_buffer.h"
 #include "hd44780.h"
 #include "select_screen.h"
+#include "set_screen.h"
+#include "screens.h"
 #include "button.h"
+
+#include "ring_buffer.h"
+RingBuffer<16> tmp;
+
 
 
 /// эта функция вызывается первой в startup файле
@@ -27,32 +34,26 @@ using Up    = mcu::PB8;
 using Down  = mcu::PB9;
 using US    = mcu::PB0; 
 
-struct Main_screen : Screen {
-    String_buffer& lcd;
-    Function<void(Callback<>)> enter_publisher;
-    Callback<> out_callback; 
+struct Flash_data {
+    uint16_t factory_number = 0;
+    UART::Settings uart_set = {
+        .parity_enable  = false,
+        .parity         = USART::Parity::even,
+        .data_bits      = USART::DataBits::_8,
+        .stop_bits      = USART::StopBits::_1,
+        .baudrate       = USART::Baudrate::BR9600,
+        .res            = 0
+    };
+    uint8_t  modbus_address        = 1;
+    uint16_t max_temperature       = 55;
+    int uv_level_min               = 40;  
+    uint16_t qty_uv_lamps          = 5;
+    uint16_t uv_level_highest      = 0x0100;
+    int      model_number          = 0;
+    uint16_t temperature_recovery  = 20; 
+} flash;
 
-    Main_screen(String_buffer& lcd, Enter_publisher enter_publisher, Out_callback out_callback)
-        : lcd             {lcd}
-        , enter_publisher {enter_publisher.value}
-        , out_callback    {out_callback.value}
-    {}
-    void init() override {
-        enter_publisher ([this]{ out_callback(); });
-        lcd.clear();
-        lcd.line(0).center() << "Главный экран";
-        lcd << "счет: " << next_line;
-    }
-    void deinit() override {
-        enter_publisher ([]{});
-    }
 
-    int placeholder {0};
-    void draw() override {
-        lcd.line(2) << placeholder++;
-    }
-
-};
 
 
 struct Menu : TickSubscriber {
@@ -61,10 +62,12 @@ struct Menu : TickSubscriber {
     Button_event& up;
     Button_event& down;
     Button_event& enter;
+    Flash_data&   flash;
+    Screen* current_screen {&main_screen};
     size_t tick_count{0};
 
-    Menu (Button_event& up, Button_event& down, Button_event& enter)
-        : up{up}, down{down}, enter{enter} 
+    Menu (Button_event& up, Button_event& down, Button_event& enter,  Flash_data& flash)
+        : up{up}, down{down}, enter{enter}, flash{flash}
     {
         tick_subscribe();
         current_screen->init();
@@ -72,34 +75,68 @@ struct Menu : TickSubscriber {
     
     Main_screen main_screen {
           lcd
-        , Enter_publisher { [this](auto c){enter.set_click_callback(c);}     }
-        , Out_callback    { [this]{ change_screen(main_select); }}
+        , Enter_event  { [this](auto c){enter.set_click_callback(c);}     }
+        , Out_callback { [this]{ change_screen(main_select); }}
+        , flash.model_number
     };
-    Select_screen<5> main_select {
+    Select_screen<4> main_select {
           lcd
-        , Up_publisher    { [this](auto c){   up.set_click_callback(c);}     }
-        , Down_publisher  { [this](auto c){ down.set_click_callback(c);}     }
-        , Enter_publisher { [this](auto c){enter.set_click_callback(c);}     }
-        , Out_publisher   { [this](auto c){enter.set_long_push_callback(c);} }
+        , Up_event    { [this](auto c){   up.set_click_callback(c);}     }
+        , Down_event  { [this](auto c){ down.set_click_callback(c);}     }
+        , Enter_event { [this](auto c){enter.set_click_callback(c);}     }
+        , Out_event   { [this](auto c){enter.set_long_push_callback(c);} }
         , Out_callback       { [this]{ change_screen(main_screen);  }}
         , Line {"Аварии"      ,[this]{ change_screen(alarm_select); }}
         , Line {"Наработка"   ,[]{}}
-        , Line {"Конфигурация",[]{}}
+        , Line {"Конфигурация",[this]{ change_screen(config_select); }}
         , Line {"Лог работы"  ,[]{}}
-        , Line {"Настройки"   ,[]{}}
     };
     Select_screen<3> alarm_select {
           lcd
-        , Up_publisher    { [this](auto c){   up.set_click_callback(c);}     }
-        , Down_publisher  { [this](auto c){ down.set_click_callback(c);}     }
-        , Enter_publisher { [this](auto c){enter.set_click_callback(c);}     }
-        , Out_publisher   { [this](auto c){enter.set_long_push_callback(c);} }
+        , Up_event    { [this](auto c){   up.set_click_callback(c);}     }
+        , Down_event  { [this](auto c){ down.set_click_callback(c);}     }
+        , Enter_event { [this](auto c){enter.set_click_callback(c);}     }
+        , Out_event   { [this](auto c){enter.set_long_push_callback(c);} }
         , Out_callback    { [this]{ change_screen(main_select);  }}
         , Line {"Нерабочие лампы"    ,[]{}}
         , Line {"Ошибки линии RS485" ,[]{}}
         , Line {"Сбросить аварии"    ,[]{}}
     };
-    Screen* current_screen {&main_screen};
+    Select_screen<4> config_select {
+          lcd
+        , Up_event    { [this](auto c){   up.set_click_callback(c);}     }
+        , Down_event  { [this](auto c){ down.set_click_callback(c);}     }
+        , Enter_event { [this](auto c){enter.set_click_callback(c);}     }
+        , Out_event   { [this](auto c){enter.set_long_push_callback(c);} }
+        , Out_callback    { [this]{ change_screen(main_select);  }}
+        , Line {"Просмотр конф.-ии" ,[]{}}
+        , Line {"Тревога УФ"        ,[this]{ change_screen(min_UV_set);   }}
+        , Line {"Настройка сети"    ,[this]{ change_screen(rs485_select); }}
+        , Line {"Тех. настройки"    ,[]{}}
+    };
+    Select_screen<4> rs485_select {
+          lcd
+        , Up_event    { [this](auto c){   up.set_click_callback(c);}     }
+        , Down_event  { [this](auto c){ down.set_click_callback(c);}     }
+        , Enter_event { [this](auto c){enter.set_click_callback(c);}     }
+        , Out_event   { [this](auto c){enter.set_long_push_callback(c);} }
+        , Out_callback    { [this]{ change_screen(config_select);  }}
+        , Line {"Адрес:     255"    ,[]{}}
+        , Line {"Скорость:  115200"     ,[]{}}
+        , Line {"Проверка:  отсутсв."   ,[]{}}
+        , Line {"Стоп биты: 1"         ,[]{}}
+    };
+    Set_screen<int> min_UV_set {
+          lcd
+        , Up_event    { [this](auto c){   up.set_click_callback(c);}     }
+        , Down_event  { [this](auto c){ down.set_click_callback(c);}     }
+        , Enter_event { [this](auto c){enter.set_click_callback(c);}     }
+        , Out_event   { [this](auto c){enter.set_long_push_callback(c);} }
+        , Out_callback    { [this]{ change_screen(config_select);  }}
+        , "уровень УФ"
+        , flash.uv_level_min
+    };
+    
 
     void notify() override {
         every_qty_cnt_call(tick_count, 50, [this]{
@@ -107,22 +144,23 @@ struct Menu : TickSubscriber {
         });
     }
 
-    void change_screen(Screen& news) {
+    void change_screen(Screen& new_screen) {
         current_screen->deinit();
-        current_screen = &news;
+        current_screen = &new_screen;
         current_screen->init();
+        auto i = new int;
+        tmp.push(uint32_t(i));
+        delete i;
     }
 };
-
-
 
 
 int main()
 {
     auto up    = Button<Up>();
     auto down  = Button<Down>();
-    auto enter = Buttons(up, down);
-    [[maybe_unused]] auto _ = Menu(up, down, enter);
+    auto enter = Tied_buttons(up, down);
+    [[maybe_unused]] auto _ = Menu(up, down, enter, flash);
 
     while (1) {
         __WFI();
---
+++ src/screens.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include "screen_common.h"
+#include <array>
+
+constexpr auto models = std::array{
+    "УОВ-ПВ-1   ",
+    "УОВ-ПВ-5   ",
+    "УОВ-ПВ-10  ",
+    "УОВ-ПВ-15  ",
+    "УОВ-ПВ-30  ",
+    "УОВ-ПВ-50  ",
+    "УОВ-ПВ-100 ",
+    "УОВ-ПВ-150 ",
+    "УОВ-ПВ-200 ",
+    "УОВ-ПВ-300 ",
+    "УОВ-ПВ-500 ",
+    "УОВ-ПВ-700 ",
+    "УОВ-ПВ-1000",
+    "УОВ-ПВ-1500",
+    "УОВ-СВ-5   ",
+    "УОВ-СВ-10  ",
+    "УОВ-СВ-15  ",
+    "УОВ-СВ-30  ",
+    "УОВ-СВ-50  ",
+    "УОВ-СВ-100 ",
+    "УОВ-СВ-150 ",
+    "УОВ-СВ-200 ",
+    "УОВ-СВ-300 ",
+    "УОВ-СВ-500 ",
+    "УОВ-СВ-700 ",
+    "УОВ-СВ-1000",
+    "УОВ-СВ-1500" 
+};
+
+
+struct Main_screen : Screen {
+    String_buffer& lcd;
+    Eventer enter_event;
+    Callback<> out_callback;
+    int& model_n;
+
+    Main_screen(
+          String_buffer& lcd
+        , Enter_event enter_event
+        , Out_callback out_callback
+        , int& model_n
+    ) : lcd          {lcd}
+      , enter_event  {enter_event.value}
+      , out_callback {out_callback.value}
+      , model_n      {model_n}
+    {}
+    void init() override {
+        enter_event ([this]{ out_callback(); });
+        lcd.clear();
+        lcd.line(0).center() << models[model_n];
+        lcd << "счет: " << next_line;
+    }
+    void deinit() override {
+        enter_event (null_function);
+    }
+
+    int placeholder {0};
+    void draw() override {
+        lcd.line(2) << placeholder++;
+    }
+};
