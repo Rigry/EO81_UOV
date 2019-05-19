4432953da2cda39ef6c606fecea16bf46246bd3e
slonegd <slonkdv@mail.ru>
2019-05-19 09:34:46 +0300

    add boudrate set to menu

--- src/menu.h
+++ src/menu.h
@@ -23,19 +23,20 @@ struct Menu : TickSubscriber {
     Screen* current_screen {&main_screen};
     size_t tick_count{0};
 
-    Menu (Pins pins, Button_event& up, Button_event& down, Button_event& enter,  Flash_data& flash)
-        : up{up}, down{down}, enter{enter}, flash{flash}
-    {
-        tick_subscribe();
-        current_screen->init();
-    }
-
     Buttons_events buttons_events {
           Up_event    { [this](auto c){   up.set_click_callback(c);}     }
         , Down_event  { [this](auto c){ down.set_click_callback(c);}     }
         , Enter_event { [this](auto c){enter.set_click_callback(c);}     }
         , Out_event   { [this](auto c){enter.set_long_push_callback(c);} }
     };
+
+
+    Menu (Pins pins, Button_event& up, Button_event& down, Button_event& enter,  Flash_data& flash)
+        : up{up}, down{down}, enter{enter}, flash{flash}
+    {
+        tick_subscribe();
+        current_screen->init();
+    }
     
     Main_screen main_screen {
           lcd
@@ -43,6 +44,7 @@ struct Menu : TickSubscriber {
         , Out_callback { [this]{ change_screen(main_select); }}
         , flash.model_number
     };
+
     Select_screen<4> main_select {
           lcd, buttons_events
         , Out_callback       { [this]{ change_screen(main_screen);  }}
@@ -51,6 +53,7 @@ struct Menu : TickSubscriber {
         , Line {"Конфигурация",[this]{ change_screen(config_select); }}
         , Line {"Лог работы"  ,[]{}}
     };
+
     Select_screen<3> alarm_select {
           lcd, buttons_events
         , Out_callback    { [this]{ change_screen(main_select);  }}
@@ -58,6 +61,7 @@ struct Menu : TickSubscriber {
         , Line {"Ошибки линии RS485" ,[]{}}
         , Line {"Сбросить аварии"    ,[]{}}
     };
+
     Select_screen<4> config_select {
           lcd, buttons_events
         , Out_callback             { [this]{ change_screen(main_select);  }}
@@ -66,22 +70,25 @@ struct Menu : TickSubscriber {
         , Line {"Настройка сети"    ,[this]{ change_screen(rs485_select); }}
         , Line {"Тех. настройки"    ,[this]{ change_screen(tech_select);  }}
     };
+
     Select_screen<4> rs485_select {
           lcd, buttons_events
-        , Out_callback    { [this]{ change_screen(config_select);  }}
-        , Line {"Адрес"       ,[this]{ change_screen(address_set);   }}
-        , Line {"Скорость"    ,[]{}}
+        , Out_callback       { [this]{ change_screen(config_select);  }}
+        , Line {"Адрес"       ,[this]{ change_screen(address_set);    }}
+        , Line {"Скорость"    ,[this]{ change_screen(boudrate_set);   }}
         , Line {"Проверка"    ,[]{}}
         , Line {"Стоп биты"   ,[]{}}
     };
+
     Select_screen<4> tech_select {
           lcd, buttons_events
-        , Out_callback    { [this]{ change_screen(config_select);  }}
+        , Out_callback        { [this]{ change_screen(config_select);  }}
         , Line {"Наименование" ,[this]{ change_screen(name_set);   }}
         , Line {"туду"   ,[]{}}
         , Line {"туду"   ,[]{}}
         , Line {"туду"   ,[]{}}
     };
+
     Set_screen<int> min_UV_set {
           lcd, buttons_events
         , Out_callback    { [this]{ change_screen(config_select);  }}
@@ -89,6 +96,7 @@ struct Menu : TickSubscriber {
         , flash.uv_level_min
         , 1, 100
     };
+
     Set_screen<uint8_t> address_set {
           lcd, buttons_events
         , Out_callback    { [this]{ change_screen(rs485_select);  }}
@@ -96,6 +104,20 @@ struct Menu : TickSubscriber {
         , flash.modbus_address
         , 1, 255
     };
+
+    // приходится сохранять, так как нельзя сделать ссылку на член битового поля
+    uint8_t boudrate_ {flash.uart_set.baudrate};
+    Set_screen<uint8_t, boudrate_to_string> boudrate_set {
+          lcd, buttons_events
+        , Out_callback    { [this]{ 
+            flash.uart_set.baudrate = USART::Baudrate(boudrate_);
+            change_screen(rs485_select);
+        }}
+        , "Скорость в бодах"
+        , boudrate_
+        , 0, ::boudrate.size()
+    };
+
     Set_screen<int, model_to_string> name_set {
           lcd, buttons_events
         , Out_callback    { [this]{ change_screen(config_select);  }}
--- src/screens.h
+++ src/screens.h
@@ -3,32 +3,32 @@
 #include "screen_common.h"
 #include <array>
 
-constexpr auto models = std::array{
-    "УОВ-ПВ-1   ",
-    "УОВ-ПВ-5   ",
-    "УОВ-ПВ-10  ",
-    "УОВ-ПВ-15  ",
-    "УОВ-ПВ-30  ",
-    "УОВ-ПВ-50  ",
-    "УОВ-ПВ-100 ",
-    "УОВ-ПВ-150 ",
-    "УОВ-ПВ-200 ",
-    "УОВ-ПВ-300 ",
-    "УОВ-ПВ-500 ",
-    "УОВ-ПВ-700 ",
+constexpr auto models = std::array {
+    "УОВ-ПВ-1",
+    "УОВ-ПВ-5",
+    "УОВ-ПВ-10",
+    "УОВ-ПВ-15",
+    "УОВ-ПВ-30",
+    "УОВ-ПВ-50",
+    "УОВ-ПВ-100",
+    "УОВ-ПВ-150",
+    "УОВ-ПВ-200",
+    "УОВ-ПВ-300",
+    "УОВ-ПВ-500",
+    "УОВ-ПВ-700",
     "УОВ-ПВ-1000",
     "УОВ-ПВ-1500",
-    "УОВ-СВ-5   ",
-    "УОВ-СВ-10  ",
-    "УОВ-СВ-15  ",
-    "УОВ-СВ-30  ",
-    "УОВ-СВ-50  ",
-    "УОВ-СВ-100 ",
-    "УОВ-СВ-150 ",
-    "УОВ-СВ-200 ",
-    "УОВ-СВ-300 ",
-    "УОВ-СВ-500 ",
-    "УОВ-СВ-700 ",
+    "УОВ-СВ-5",
+    "УОВ-СВ-10",
+    "УОВ-СВ-15",
+    "УОВ-СВ-30",
+    "УОВ-СВ-50",
+    "УОВ-СВ-100",
+    "УОВ-СВ-150",
+    "УОВ-СВ-200",
+    "УОВ-СВ-300",
+    "УОВ-СВ-500",
+    "УОВ-СВ-700",
     "УОВ-СВ-1000",
     "УОВ-СВ-1500" 
 };
@@ -36,6 +36,40 @@ constexpr auto models = std::array{
 constexpr std::string_view model_to_string(int i) {
     return models[i];
 }
+
+constexpr auto exist = std::array {
+    "отсутствует",
+    "присутствует"
+};
+
+constexpr std::string_view exist_to_string(int i) {
+    return exist[i];
+}
+
+constexpr auto boudrate = std::array {
+    "9600",
+    "14400"
+    "19200",
+    "28800",
+    "38400",
+    "57600",
+    "76800",
+    "115200"
+};
+
+constexpr std::string_view boudrate_to_string(int i) {
+    return boudrate[i];
+}
+
+constexpr auto parity = std::array {
+    "нечетность",
+    "четность  "  
+};
+
+constexpr std::string_view parity_to_string(int i) {
+    return parity[i];
+}
+
 
 
 struct Main_screen : Screen {
